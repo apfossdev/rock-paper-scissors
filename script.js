029094cd910d2b9be4b577a4381ce856279ae72a
Prabu <annamalaiprabudev@gmail.com>
2024-03-09 21:30:37 +0530

    Built UI for RPS Game using JS DOM Manipulation

--- script.js
+++ script.js
@@ -3,72 +3,92 @@ let playerScore = 0;
 let computerScore = 0;
 
 
-function getComputerChoice(){
+function computerSelection() {
     let randomNum = Math.floor(Math.random() * 3);
     return options[randomNum];
-}
+};
 
 function playRound(playerSelection, computerSelection) {
-
-    playerSelection = playerSelection.toUpperCase();
-
-    if ((playerSelection == "ROCK" && computerSelection == "ROCK") || (playerSelection == "PAPER" && computerSelection == "PAPER") || (playerSelection == "SCISSORS" && computerSelection == "SCISSORS")){
-        return "You Draw!";
+    
+    if(playerScore == 5 || computerScore == 5) {
+        if(playerScore == 5) {
+            roundResult.textContent = "You have won against the computer, just wow :)";
+        }
+        else {
+            roundResult.textContent = "You have lost to the computer, just wow :(";
+        }  
     }
-
-    else if (playerSelection == "ROCK" && computerSelection == "SCISSORS"){
-        playerScore++;
-        return "You Win! Rock beats Scissors";
-    }
-
-    else if (playerSelection == "ROCK" && computerSelection == "PAPER") {
-        computerScore++;
-        return "You Lose! Paper beats Rock"
-    }
-
-    else if (playerSelection == "PAPER" && computerSelection == "SCISSORS"){
-        computerScore++;
-        return "You Lose! Scissors beats Paper";
-    }
-
-    else if (playerSelection == "PAPER" && computerSelection == "ROCK") {
-        playerScore++;
-        return "You Win! Paper beats Rock"
-    }
-
-    else if (playerSelection == "SCISSORS" && computerSelection == "ROCK"){
-        computerScore++;
-        return "You Lose! Rock beats Scissors";
-    }
-
-    else if (playerSelection == "SCISSORS" && computerSelection == "PAPER") {
-        playerScore++;
-        return "You Win! Scissors beats Paper";
+    
+    else {
+        if ((playerSelection == "ROCK" && computerSelection == "ROCK") || (playerSelection == "PAPER" && computerSelection == "PAPER") || (playerSelection == "SCISSORS" && computerSelection == "SCISSORS")){
+            roundResult.textContent = "You draw!";
+            resultDiv.textContent = "Player Score: " + playerScore + " | " + "Computer Score " + computerScore;
+            return "You Draw!";
+        }
+    
+        else if (playerSelection == "ROCK" && computerSelection == "SCISSORS"){
+            playerScore++;
+            roundResult.textContent = "You Win! Rock beats Scissors";
+            resultDiv.textContent = "Player Score: " + playerScore + " | " + "Computer Score " + computerScore;
+            return "You Win! Rock beats Scissors";
+        }
+    
+        else if (playerSelection == "ROCK" && computerSelection == "PAPER") {
+            computerScore++;
+            roundResult.textContent = "You Lose! Paper beats Rock";
+            resultDiv.textContent = "Player Score: " + playerScore + " | " + "Computer Score " + computerScore;
+            return "You Lose! Paper beats Rock"
+        }
+    
+        else if (playerSelection == "PAPER" && computerSelection == "SCISSORS"){
+            computerScore++;
+            roundResult.textContent = "You Lose! Scissors beats Paper";
+            resultDiv.textContent = "Player Score: " + playerScore + " | " + "Computer Score " + computerScore;
+            return "You Lose! Scissors beats Paper";
+        }
+    
+        else if (playerSelection == "PAPER" && computerSelection == "ROCK") {
+            playerScore++;
+            roundResult.textContent = "You Win! Paper beats Rock";
+            resultDiv.textContent = "Player Score: " + playerScore + " | " + "Computer Score " + computerScore;
+            return "You Win! Paper beats Rock"
+        }
+    
+        else if (playerSelection == "SCISSORS" && computerSelection == "ROCK"){
+            computerScore++;
+            roundResult.textContent = "You Lose! Rock beats Scissors";
+            resultDiv.textContent = "Player Score: " + playerScore + " | " + "Computer Score " + computerScore;
+            return "You Lose! Rock beats Scissors";
+        }
+    
+        else if (playerSelection == "SCISSORS" && computerSelection == "PAPER") {
+            playerScore++;
+            roundResult.textContent = "You Win! Scissors beats Paper";
+            resultDiv.textContent = "Player Score: " + playerScore + " | " + "Computer Score " + computerScore;
+            return "You Win! Scissors beats Paper";
+        }
     }
     
 }
 
+let rockButton = document.getElementById("rockButton");
+let paperButton = document.getElementById("paperButton");
+let scissorsButton = document.getElementById("scissorsButton");
 
-function playGame() {
+rockButton.addEventListener("click",function () {
+    playRound("ROCK", computerSelection());
+});
+paperButton.addEventListener("click",function () {
+    playRound("PAPER", computerSelection());
+});
+scissorsButton.addEventListener("click",function () {
+    playRound("SCISSORS", computerSelection());
+});
 
-    for(let i=0; i<5; i++){
-        let playerSelection = prompt("Choose Rock, Paper or Scissors...");
-        let computerSelection = getComputerChoice();   
-        console.log(playRound(playerSelection, computerSelection)); 
-        console.log(playerSelection);
-        console.log(computerSelection);
-        console.log("Player Score:"+ playerScore);
-        console.log("Computer Score:"+ computerScore);
-    }
-    if (playerScore > computerScore){
-        console.log("You Win :)");
-    }
-    else if(playerScore == computerScore){
-        console.log("It's a Draw:|");
-    }
-    else {
-        console.log("You Lose :(");
-    }
-}
+let container = document.getElementById('container');
+let roundResult = document.createElement('div');
+let resultDiv = document.createElement('div');
 
-playGame();
+
+container.appendChild(roundResult);
+container.appendChild(resultDiv);
